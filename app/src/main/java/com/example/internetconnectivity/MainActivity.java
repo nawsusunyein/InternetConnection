5b4a4a5db2bb321c8ecd13d4a24f17caacdb83b8
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-08 17:29:01 +0630

    check network connectivity

--- app/src/main/java/com/example/internetconnectivity/MainActivity.java
+++ app/src/main/java/com/example/internetconnectivity/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.internetconnectivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showNetworkStatus(View view){
+        if(isConnected()){
+            Toast.makeText(this,"Network is connected",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Network is disconnected",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isConnected() {
+        boolean connected = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo nInfo = cm.getActiveNetworkInfo();
+            connected = nInfo != null && nInfo.isAvailable() && nInfo.isConnected();
+            return connected;
+        } catch (Exception e) {
+            Log.e("Connectivity Exception", e.getMessage());
+        }
+        return connected;
     }
 }
